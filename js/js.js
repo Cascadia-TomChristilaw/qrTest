ad51830dc978a8bdb06d0c695290d8142fa70453
Tom Christilaw <tomchristilaw@student.cascadia.edu>
2013-05-07 19:57:24 -0700

    deleted old commented code blocks

--- js/js.js
+++ js/js.js
@@ -1,12 +1,3 @@
-/*$(document).ready(function() {
-	$("#scanButton").click(function() {
-		scan( function (result) { 
-			alert("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled); 
-		});
-	});
-});
-*/
-
 $(document).ready(function() {
 	$("#scanButton").click(function() {
 		scan();
@@ -24,20 +15,3 @@ var scan = function() {
         alert("Scan failed: " + error);
     });
 }
-
-/*function scan(successFunction) {
-    window.plugins.barcodeScanner.scan(function (result) {
-          if (!result.cancelled) {
-            // Successfully scanned a bar code
-            if (result.format == "CODE_128") {
-              loadCardData(result.text, successFunction);
-            } else {
-              $('#typeInPopup #hint').html("Sorry, invalid format");
-              $('#typeInPopup').popup('open', { transition: 'pop'});
-            }
-          }
-        }, function (error) {
-          alert("Scanning failed: " + error);
-        }
-    );
-}*/
